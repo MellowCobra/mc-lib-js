6764ebd412b2074e1bc7bcc24abb6365bcd41bfc
Grayson Dubois <grayson.dubois@hcahealthcare.com>
2021-10-20 11:07:42 -0500

    doc update

--- src/function.ts
+++ src/function.ts
@@ -10,7 +10,7 @@ export type AnyFunction = (...args: any[]) => any
  * the first argument and returning a function taking the next argument, 
  * down to N arguments.
  * 
- * Does not work with 
+ * Does not work with variable argument functions
  * 
  * This enables partial application
  * 

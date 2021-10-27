533f5081d969f08bc2357e59babba483f91496f2
Grayson Dubois <grayson.dubois@hcahealthcare.com>
2021-10-27 12:57:41 -0500

    More object methods

--- src/object.ts
+++ src/object.ts
@@ -53,11 +53,131 @@ export const getIn = curry(function (...args: [any] | [...any[], any]): any | nu
   return getIn(...rest, next)
 })
 
+/**
+ * Returns true or false if value passed in is an empty object.
+ * 
+ * Non-object inputs yield a null return value
+ * 
+ * @param { any } obj the object to check
+ * @returns { any | null }
+ * 
+ * Usage:
+ * ```typescript
+ * Obj.isEmpty({})                // true
+ * Obj.isEmpty({ a: "hello" })    // false
+ * Obj.isEmpty(1)                 // null
+ * ```
+ */
+export const isEmpty = curry(function isEmpty(obj: {}): boolean | null {
+  if (obj == null || typeof obj !== 'object' || Array.isArray(obj)) return null
 
+  return Object.keys(obj).length === 0
+})
 
-// Obj.keys
-// Obj.isEmpty
-// Obj.values
-// Obj.entries
-// Obj.withoutKeys
-// Obj.onlyKeys
+/**
+ * Returns a new object that has the specified fields omitted
+ * 
+ * Non-object inputs yield a null return value
+ * 
+ * @param { any } obj the object to check
+ * @returns { any | null }
+ * 
+ * Usage:
+ * ```typescript
+ * Obj.withoutKeys(["age"], { name: "Brian", age: 25, gender: "M" })  // { name: "Brian", gender: "M" }
+ * ```
+ */
+export const withoutKeys = curry(function withoutKeys(keys: [string], obj: {}): any {
+  if (obj == null || typeof obj !== "object" || Array.isArray(obj)) return null
+
+  return Object.entries(obj)
+    .reduce((newObj, [k, v]) => {
+      if (!Arr.contains(k, keys)) newObj[k] = v
+
+      return newObj
+    }, {})
+})
+
+/**
+ * Returns a new object that has only the specified fields
+ * 
+ * Non-object inputs yield a null return value
+ * 
+ * @param { any } obj the object to check
+ * @returns { any | null }
+ * 
+ * Usage:
+ * ```typescript
+ * Obj.onlyKeys(["name", "age"], { name: "Brian", age: 25, gender: "M" })  // { name: "Brian", age: 25 }
+ * ```
+ */
+export const onlyKeys = curry(function onlyKeys(keys: [string], obj: {}): any{
+  if (obj == null || typeof obj !== "object" || Array.isArray(obj)) return null
+
+  return Object.entries(obj)
+    .reduce((newObj, [k, v]) => {
+      if (Arr.contains(k, keys)) newObj[k] = v
+
+      return newObj
+    }, {})
+})
+
+/**
+ * Wrapper around the Object.keys method.
+ * Returns an array of strings specifying the keys on an object.
+ * 
+ * @param { any } obj the object to check
+ * @returns { [string] | null }
+ * 
+ * Usage:
+ * ```typescript
+ * Obj.keys({ name: "Brian", age: 25, gender: "M" })  // ["name", "age", "gender"]
+ * ```
+ */
+export const keys = curry(Object.keys)
+
+/**
+ * Wrapper around the Object.values method.
+ * Returns an array of the values inside an object.
+ * 
+ * @param { any } obj the object to check
+ * @returns { [any] | null }
+ * 
+ * Usage:
+ * ```typescript
+ * Obj.values({ name: "Brian", age: 25, gender: "M" })  // ["Brian", 25, "M"]
+ * ```
+ */
+export const values = curry(Object.values)
+
+/**
+ * Wrapper around the Object.entries method.
+ * Returns an array of arrays where each sub array maps to a key:value pair; the first element being the key, and the second element being the value.
+ * 
+ * @param { any } obj the object to check
+ * @returns { [ string, any ] | null }
+ * 
+ * Usage:
+ * ```typescript
+ * Obj.keys({ name: "Brian", age: 25, gender: "M" })  // ["name", "age", "gender"]
+ * ```
+ */
+export const entries = curry(Object.entries)
+
+/**
+ * Wrapper around the Object.freeze method.
+ * Returns a frozen instance of the object passed in. 
+ * Attempts to modify this new frozen object will throw an erro
+ * 
+ * @param { any } obj the object to check
+ * @returns { [ string, any ] | null }
+ * 
+ * Usage:
+ * ```typescript
+ * const frozenBrian = Obj.freeze({ name: "Brian", age: 25, gender: "M" })
+ * 
+ * frozenBrian.age = 26           // Throws error
+ * console.log(frozenBrian.age)   // 25
+ * ```
+ */
+export const freeze = curry(Object.freeze)
--- test/unit/object.test.ts
+++ test/unit/object.test.ts
@@ -63,4 +63,122 @@ describe("Object Module", () => {
       expect(Obj.getIn("a", "b", "c", object)).toBeNull()
     })
   })
+
+  describe("keys", () => {
+    test("on an empty object, returns empty array", () => {
+      expect(Obj.keys({})).toEqual([])
+    })
+
+    test("on an non-nested object, returns an array of keys", () => {
+      expect(Obj.keys({ a: "hello", b: "my name is"})).toEqual([ 'a', 'b' ])
+    })
+
+    test("on nested object, only show top-level keys", () => {
+      expect(Obj.keys({ a: { b: 'c' } })).toEqual(['a'])
+    })
+
+    test("keys are strings", () => {
+      expect(Obj.keys({0: 0, 1: 1})).toEqual(['0', '1'])
+    })
+  })
+
+  describe("values", () => {
+    test("on an empty object, returns an empty array", () => {
+      expect(Obj.values({})).toEqual([])
+    })
+
+    test("on a non-nested object, returns an array of the values", () => {
+      expect(Obj.values({ a: "hello", b: "my name is"})).toEqual(["hello", "my name is"])
+    })
+
+    test("on nested object, returns values included entire nested values", () => {
+      expect(Obj.values({ a: { b: 'c' } })).toEqual([{ b: 'c' }])
+    })
+  })
+
+  describe("freeze", () => {
+    test("attempting to modify frozen objects throws error", () => {
+      const dinner = { entree: "chicken parmesan", servings: 7 }
+      const frozenDinner = Obj.freeze(dinner)
+      expect(() => {
+        frozenDinner.servings = 8
+        expect(frozenDinner.servings).toEqual(7)
+      }).toThrow()
+    })
+  })
+
+  describe("isEmpty", () => {
+    test("true for empty objects", () => {
+      expect(Obj.isEmpty({})).toBe(true)
+    })
+
+    test("false for non-empty object", () => {
+      expect(Obj.isEmpty({ a: 1 })).toBe(false)
+    })
+
+    test("null for empty array", () => {
+      expect(Obj.isEmpty([])).toBeNull()
+    })
+
+    test("null for non-empty array", () => {
+      expect(Obj.isEmpty([1,2,3])).toBeNull()
+    })
+
+    test.each([1, null, undefined, "abc", () => {}])("null for %s", (value) => {
+      expect(Obj.isEmpty(value)).toBeNull()
+    })
+  })
+
+  describe("withoutKeys", () => {
+    const brian = { name: "Brian", age: 25, gender: "M" }
+    
+    test.each([1, null, undefined, "abc", () => {}])("null for %s", (value) => {
+      expect(Obj.withoutKeys(["a"], value)).toBeNull()
+    })
+
+    test("empty object for empty object input", () => {
+      expect(Obj.withoutKeys(['a'], {})).toEqual({})
+    })
+
+    test("not passing in keys yields same object", () => {
+      
+      expect(Obj.withoutKeys([], brian))
+    })
+
+    test("yields new object with specified keys omitted", () => {
+      expect(Obj.withoutKeys(["age", "gender"], brian)).toEqual({ name: "Brian" })
+    })
+
+    test("can be curried", () => {
+      const removeGenderField = Obj.withoutKeys(["gender"])
+
+      expect(removeGenderField(brian)).toEqual({ name: "Brian", age: 25 })
+    })
+  })
+
+  describe("onlyKeys", () => {
+    const gertie = { name: "Gertrude", age: 32, gender: "F" }
+
+    test.each([1, null, undefined, "abc", () => {}])("null for %s", (value) => {
+      expect(Obj.onlyKeys(['a'], value)).toBeNull()
+    })
+
+    test("empty object for empty input", () => {
+      expect(Obj.onlyKeys(["a"], {})).toEqual({})
+    })
+
+    test("not passing in keys yields an empty object", () => {
+      expect(Obj.onlyKeys([], gertie)).toEqual({})
+    })
+
+    test("yields new objects with only the specified keys", () => {
+      expect(Obj.onlyKeys(["name", "gender"], gertie)).toEqual({ name: "Gertrude", gender: "F" })
+    })
+
+    test("can be curried", () => {
+      const nameAndAge = Obj.onlyKeys(["name", "age"])
+
+      expect(nameAndAge(gertie)).toEqual({ name: "Gertrude", age: 32 })
+    })
+  })
 })
